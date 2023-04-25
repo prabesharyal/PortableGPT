6419e9910c85dff5f77cec1df5cea7bd3976c7ec
Prabesh Aryal <73214060+prabesharyal@users.noreply.github.com>
2023-04-25 22:51:02 +0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-message_history = {456:[{"role": "system", 123 : "hello", "content": "You are a helpful assistant"}]}
-new = {"role": "system", "content": "hi"}
-message_history[456].append(new)
-print(message_history)
-var = 123
-if var in message_history:
-    print("hi")
